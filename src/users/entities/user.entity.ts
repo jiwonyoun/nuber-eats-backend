6501c270d4e414e0a17a48f239d58ebfc1eeee52
jiwonyoun <jwy9724@gmail.com>
2021-04-07 16:31:32 +0900

    add hashing password method

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,46 @@
+import {
+  Field,
+  InputType,
+  ObjectType,
+  registerEnumType,
+} from '@nestjs/graphql';
+import { CoreEntity } from 'src/common/entities/core.entity';
+import { BeforeInsert, Column, Entity } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { InternalServerErrorException } from '@nestjs/common';
+
+enum UserRole {
+  Owner,
+  Client,
+  Delivery,
+}
+
+registerEnumType(UserRole, { name: 'UserRole' });
+
+@Entity()
+@InputType({ isAbstract: true })
+@ObjectType()
+export class User extends CoreEntity {
+  @Column()
+  @Field(() => String)
+  email: string;
+
+  @Column()
+  @Field(() => String)
+  password: string;
+
+  @Column({ type: 'enum', enum: UserRole })
+  @Field(() => UserRole)
+  role: UserRole;
+
+  // Repository에 save 되기 전에 실행되는 method
+  @BeforeInsert()
+  async hashPassword(): Promise<void> {
+    try {
+      this.password = await bcrypt.hash(this.password, 10);
+    } catch (e) {
+      console.log(e);
+      throw new InternalServerErrorException();
+    }
+  }
+}
---
+++ src/users/users.resolver.ts
@@ -0,0 +1,41 @@
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import {
+  CreateAccountInput,
+  CreateAccountOutput,
+} from './dtos/create-account.dto';
+import { User } from './entities/user.entity';
+import { UserService } from './users.service';
+
+@Resolver(() => User)
+export class UserResolver {
+  constructor(private readonly usersService: UserService) {}
+
+  @Query(() => Boolean)
+  hi() {
+    return true;
+  }
+
+  @Mutation(() => CreateAccountOutput)
+  async createAccount(
+    @Args('input') createAccountInput: CreateAccountInput,
+  ): Promise<CreateAccountOutput> {
+    try {
+      const error = await this.usersService.createAccount(createAccountInput);
+      if (error) {
+        return {
+          ok: false,
+          error,
+        };
+      } else {
+        return {
+          ok: true,
+        };
+      }
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
+    }
+  }
+}
